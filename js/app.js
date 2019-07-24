8c24d6b2cf5d263707da8123a151b2d0da3beeb9
js-tut <johnsmilga@gmail.com>
2019-07-24 11:02:51 -0700

    finished project

--- js/app.js
+++ js/app.js
@@ -12,4 +12,39 @@ const links = document.getElementById("nav-links");
 navBtn.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
+// ********** navbar fixed ************
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 62) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
+});
+
 // ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    // prevent default
+    e.preventDefault();
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //
+    let position;
+    if (navbar.classList.contains("fixed")) {
+      position = element.offsetTop - 62;
+    } else {
+      position = element.offsetTop - 124;
+    }
+
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: position,
+      behavior: "smooth"
+    });
+  });
+});
