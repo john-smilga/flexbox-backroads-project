3f5f8f8ea43926874dd7261aa33609a792ba3be8
js-tut <johnsmilga@gmail.com>
2019-07-24 10:20:28 -0700

    everything apart from smooth-scroll

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
-// set date
+// ********** set date ************
+// select span
 const date = (document.getElementById(
   "date"
 ).innerHTML = new Date().getFullYear());
+
+// ********** nav toggle ************
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+// add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+// ********** smooth scroll ************
